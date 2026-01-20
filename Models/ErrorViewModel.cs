2b73ecbe0ce76d9245b4a8cbee37a1db0831a2a3
vinay <vinay@VINAY>
2026-01-20 11:30:59 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bubble_Game.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ wwwroot/js/Bubble.js
@@ -0,0 +1,7 @@
+﻿var clutter = "";
+
+for (var i = 1; i <= 168; i++) {
+    clutter += `<div class="bubble">1</div>`;     
+}
+
+document.querySelector('#Pbtm').innerHTML = clutter;
